9942a04e556c83b7f09f1bb282b96cf07e85ab7d
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2022-08-09 18:43:17 +0900

    Fixed minor bugs in imports

--- src/dcore/impurity_solvers/__init__.py
+++ src/dcore/impurity_solvers/__init__.py
@@ -1,7 +1,7 @@
 from .triqs_cthyb import TRIQSCTHYBSolver
 from .triqs_hubbard_I import TRIQSHubbardISolver
 from .alps_cthyb import ALPSCTHYBSolver
-#from .alps_cthyb_v2 import ALPSCTHYBSolver_v2
+from .alps_cthyb_v2 import ALPSCTHYBSolver_v2
 from .null_solver import NullSolver
 from .alps_cthyb_seg import ALPSCTHYBSEGSolver
 from .pomerol import PomerolSolver
@@ -12,7 +12,7 @@ solver_classes = {
     'TRIQS/cthyb': TRIQSCTHYBSolver,
     'TRIQS/hubbard-I': TRIQSHubbardISolver,
     'ALPS/cthyb': ALPSCTHYBSolver,
-    #'ALPS/cthyb_v2': ALPSCTHYBSolver_v2,
+    'ALPS/cthyb_v2': ALPSCTHYBSolver_v2,
     'null': NullSolver,
     'ALPS/cthyb-seg': ALPSCTHYBSEGSolver,
     'pomerol': PomerolSolver,
